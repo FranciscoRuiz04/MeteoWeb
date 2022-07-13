da2188aab4dc01ad94433870035abe429ba8dcd4
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-07-13 18:33:37 -0500

    Update

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import pandas as pd
 import gps4cast as fc
 from dotenv import load_dotenv as env
 import os
+import concurrent.futures
 
 env()
 
@@ -9,21 +10,24 @@ _main = os.getenv('starturl')
 _urls = fc.get_linked_urls(_main)
 
 
-def tab4cast(url):
-    data = fc.DailyForecast(url)
+def tab4cast(url, df, index=0):
     try:
+        data = fc.DailyForecast(url)
         data.predict()
     except:
         dfr = [None for _ in range(7)]
     else:
         wspeed = str(data.wind[0]) + ' ' + str(data.wind[1])
         p = str(data.precip[0]) + ' - ' + str(data.precip[1])
-        dfr = [data.date, data.temp[0], data.temp[1],
-            wspeed, p, data.sun, data.prev]
+        df.loc[index] = [data.date, data.temp[0], data.temp[1],
+                         wspeed, p, data.sun, data.prev]
+        # dfr = [data.date, data.temp[0], data.temp[1],
+        #     wspeed, p, data.sun, data.prev]
     return dfr
+
+# mydf = pd.DataFrame(columns=["Date", "Tmin", "Tmax", "WSpeed", "Precip", "Sun", "Prev"])
 
 
 
 if __name__ == '__main__':
     ini = tab4cast(_main)
-    print(ini)
