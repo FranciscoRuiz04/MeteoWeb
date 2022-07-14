f27507b8a4225b5cacbb035f00bddca9c26e9890
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-07-14 15:48:36 -0500

    Create csv file. To improve it

--- index.py
+++ index.py
@@ -5,12 +5,13 @@ import gps4cast as fc
 from dotenv import load_dotenv as env
 import os
 from concurrent.futures import ThreadPoolExecutor
+from datetime import date
 #--------------------------------------------------------------#
 
 env()  # Get constant values from .env file
 
-_main = os.getenv('starturl')
-_urls = fc.get_linked_urls(_main)
+# _main = os.getenv('starturl')
+# _urls = fc.get_linked_urls(_main)
 
 ########################    Functions    ########################
 
@@ -26,10 +27,11 @@ def decorator(function):
         except:
             dfr = [None for _ in range(7)]
         else:
-            wspeed = str(data.wind[0]) + ' ' + str(data.wind[1])
-            p = str(data.precip[0]) + ' - ' + str(data.precip[1])
+            data.wind = str(data.wind[0]) + ' ' + str(data.wind[1])
+            data.precip = str(data.precip[0]) + ' - ' + str(data.precip[1])
+
             dfr = [data.date, data.temp[0], data.temp[1],
-                   wspeed, p, data.sun, data.prev]
+                   data.wind, data.precip, data.sun, data.prev]
         return dfr
     return tab4cast
 
@@ -54,7 +56,7 @@ def lastTab(url):
     return data
 
 
-def run():
+def run(mainurl):
     """
     Extract a dataframe type object with all the information about
     daily forecast ordered by date, using multiple-threads with
@@ -62,18 +64,28 @@ def run():
     """
     recs = []
     with ThreadPoolExecutor(max_workers=3) as exec:
-        for n, url in enumerate(_urls):
+        for n, url in enumerate(fc.get_linked_urls(mainurl)):
             if n == 6:
                 result = exec.submit(lastTab, url).result()
             else:
                 result = exec.submit(normTab, url).result()
             recs.append(result)
     df = pd.DataFrame(data=recs, columns=[
-                      "Date", "Tmin", "Tmax", "WSpeed", "Precip", "Sun", "Prev"])
+                      "Date", "Tmin(°C)", "Tmax(°C)", "WSpeed(km/h)", "Precip(mm)", "Sun(hrs)", "Prev"])
     df.sort_values(by='Date', ascending=True)
     return df
+
+
+def toFile(path, format, mainurl):
+    content = run(mainurl)
+    name = date.today().strftime("%b-%d-%Y")
+    file = path + os.sep + name + '.' + format
+    content.to_csv(file, encoding='utf-8', index=False)
 #--------------------------------------------------------------#
 
 
 if __name__ == '__main__':
-    print(run())
+    import sys
+    toFile(sys.argv[1], sys.argv[2], sys.argv[3])
+    # print(sys.argv)
+    # # dat.to_csv('')
