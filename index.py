4f282a008ca2ef79ec866d7117e6205039fcf536
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-07-14 17:34:37 -0500

    Fix pathfiel outcome error

--- index.py
+++ index.py
@@ -1,13 +1,12 @@
 import gps2df
 from datetime import datetime
 import os
-import sys
 
-def toFile(path, format, mainurl):
+def toFile(path, mainurl, format='txt'):
     content = gps2df.run(mainurl)
     name = datetime.now().strftime("%b-%d-%Y-%H_%M")
     file = path + os.sep + name + '.' + format
     content.to_csv(file, encoding='utf-8', index=False)
 
-if __name__=='__main__':
-    toFile(sys.argv[1], sys.argv[2], sys.argv[3])
+# if __name__=='__main__':
+toFile(os.getenv('mypath'), os.getenv('starturl'))
