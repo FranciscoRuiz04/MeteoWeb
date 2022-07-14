c58f5b2cf559e4d21d3bbfe82e69342c30d57ac3
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-07-14 14:01:08 -0500

    dataframe generation with thread pool

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import pandas as pd
 import gps4cast as fc
 from dotenv import load_dotenv as env
 import os
-import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
 
 env()
 
@@ -10,24 +10,48 @@ _main = os.getenv('starturl')
 _urls = fc.get_linked_urls(_main)
 
 
-def tab4cast(url, df, index=0):
-    try:
-        data = fc.DailyForecast(url)
-        data.predict()
-    except:
-        dfr = [None for _ in range(7)]
-    else:
-        wspeed = str(data.wind[0]) + ' ' + str(data.wind[1])
-        p = str(data.precip[0]) + ' - ' + str(data.precip[1])
-        df.loc[index] = [data.date, data.temp[0], data.temp[1],
-                         wspeed, p, data.sun, data.prev]
-        # dfr = [data.date, data.temp[0], data.temp[1],
-        #     wspeed, p, data.sun, data.prev]
-    return dfr
+def decorator(function):
+    def tab4cast(url):
+        try:
+            data = function(url)
+            data.predict()
+        except:
+            dfr = [None for _ in range(7)]
+        else:
+            wspeed = str(data.wind[0]) + ' ' + str(data.wind[1])
+            p = str(data.precip[0]) + ' - ' + str(data.precip[1])
+            dfr = [data.date, data.temp[0], data.temp[1],
+                   wspeed, p, data.sun, data.prev]
+        return dfr
+    return tab4cast
 
-# mydf = pd.DataFrame(columns=["Date", "Tmin", "Tmax", "WSpeed", "Precip", "Sun", "Prev"])
 
+@decorator
+def normTab(url):
+    data = fc.Daily4cast(url)
+    return data
+
+
+@decorator
+def lastTab(url):
+    data = fc.Last4cast(url)
+    return data
+
+
+def run():
+    recs = []
+    with ThreadPoolExecutor(max_workers=3) as exec:
+        for n, url in enumerate(_urls):
+            if n == 6:
+                result = exec.submit(lastTab, url).result()
+            else:
+                result = exec.submit(normTab, url).result()
+            recs.append(result)
+    df = pd.DataFrame(data=recs, columns=[
+                      "Date", "Tmin", "Tmax", "WSpeed", "Precip", "Sun", "Prev"])
+    df.sort_values(by='Date', ascending=True)
+    return df
 
 
 if __name__ == '__main__':
-    ini = tab4cast(_main)
+    print(run())
