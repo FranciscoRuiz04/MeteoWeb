182c9517dd182cf0352de2324ef78e183b0b50d8
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-07-28 16:16:13 -0500

    New logo

--- FRONTEND/main.py
+++ FRONTEND/main.py
@@ -13,35 +13,36 @@ from tkinter import LEFT, RIGHT, ttk
 import os
 #-----------------------    GPS Pckgs    ----------------------#
 from FRONTEND import commands
+# import commands
 #--------------------------------------------------------------#
 
 # Root window
 root = tk.Tk()
 root.geometry('850x600')
 root.resizable(False, False)
-root.config(bg='#002366')
+root.config(bg='#818284')
 root.title('MeteoWeb')
 logo = tk.PhotoImage(file=os.getenv('logopath'))
 root.iconphoto(True, logo)
 
 ######################       New Location    ##########################
 div1 = tk.LabelFrame(root,
-                     bg='#003153',
+                     bg='#134351',
                      relief='flat')
 div1.pack(fill='x', padx=10, pady=10)
 
 header1 = tk.Label(div1,
                    text='Nueva Locación',
                    font=('Arial', 10, 'bold'),
-                   bg='#003153',
-                   fg='#F1C40F')
+                   bg='#134351',
+                   fg='#FFBD08')
 header1.grid(row=0, column=0, padx=10, pady=3)
 
 # URL
 urlLab = tk.Label(div1,
                   text='URL',
                   font=('Arial', 11, 'bold'),
-                  bg='#003153',
+                  bg='#134351',
                   foreground='#dcdcdc',
                   padx=10)
 urlLab.grid(row=1, column=0, pady=7)
@@ -57,7 +58,7 @@ ent.grid(row=1, column=1)
 targetLab = tk.Label(div1,
                      text='Carpeta',
                      font=('Arial', 11, 'bold'),
-                     bg='#003153',
+                     bg='#134351',
                      foreground='#dcdcdc',
                      padx=10)
 targetLab.grid(row=2, column=0, pady=7)
@@ -73,7 +74,7 @@ ent2.grid(row=2, column=1)
 cityLab = tk.Label(div1,
                    text='Nombre',
                    font=('Arial', 11, 'bold'),
-                   bg='#003153',
+                   bg='#134351',
                    foreground='#dcdcdc',
                    padx=10)
 cityLab.grid(row=3, column=0, pady=7)
@@ -91,8 +92,8 @@ btn1 = tk.Button(div1,
                  font=('Arial', 12, 'bold'),
                  border=3,
                  relief='ridge',
-                 bg='#002366',
-                 fg='#F1C40F',
+                 bg='#134351',
+                 fg='#FFBD07',
                  width=8,
                  command=lambda: commands.addCommand(ent.get(), ent2.get(), ent3.get()))
 btn1.grid(row=4, column=1, pady=10)
@@ -100,28 +101,28 @@ btn1.grid(row=4, column=1, pady=10)
 
 ######################       Separator    ##########################
 
-div2 = tk.LabelFrame(root, bg='#F1C40F', height=3, relief='flat')
+div2 = tk.LabelFrame(root, bg='#134351', height=3, relief='flat')
 div2.pack(padx=6, fill='x')
 
 
 ######################       Drop Location    ##########################
 div3 = tk.LabelFrame(root,
-                     bg='#003153',
+                     bg='#134351',
                      relief='flat')
 div3.pack(fill='x', padx=10, pady=10)
 
 header2 = tk.Label(div3,
                    text='Borrar Locación',
                    font=('Arial', 10, 'bold'),
-                   bg='#003153',
-                   fg='#F1C40F')
+                   bg='#134351',
+                   fg='#FFBD08')
 header2.grid(row=0, column=0, padx=10, pady=3)
 
 # Cityname
 namLab = tk.Label(div3,
                   text='Nombre',
                   font=('Arial', 11, 'bold'),
-                  bg='#003153',
+                  bg='#134351',
                   foreground='#dcdcdc',
                   padx=10)
 namLab.grid(row=1, column=0, pady=7)
@@ -140,8 +141,8 @@ btn2 = tk.Button(div3,
                  font=('Arial', 12, 'bold'),
                  border=3,
                  relief='ridge',
-                 bg='#002366',
-                 fg='#F1C40F',
+                 bg='#134351',
+                 fg='#FFBD07',
                  width=8,
                  command=lambda: commands.dropCommand(ent4.get()))
 btn2.grid(row=4, column=1, pady=10)
@@ -149,10 +150,10 @@ btn2.grid(row=4, column=1, pady=10)
 
 ######################       Separator    ##########################
 
-div4 = tk.LabelFrame(root, bg='#F1C40F', height=3, relief='flat')
+div4 = tk.LabelFrame(root, bg='#134351', height=3, relief='flat')
 div4.pack(padx=6, fill='x')
 
-div7 = tk.LabelFrame(root, bg='#002366', height=10, relief='flat')
+div7 = tk.LabelFrame(root, bg='#858784', height=10, relief='flat')
 div7.pack(padx=6, fill='x')
 
 
@@ -160,15 +161,15 @@ div7.pack(padx=6, fill='x')
 ######################          Header        ##########################
 
 div5 = tk.LabelFrame(root,
-                     bg='#003153',
+                     bg='#134351',
                      relief='flat')
 div5.pack(fill='x', padx=10)
 
 header3 = tk.Label(div5,
                    text='Nombre',
                    font=('Arial', 10, 'bold'),
-                   bg='#003153',
-                   fg='#F1C40F',
+                   bg='#134351',
+                   fg='#FFBB05',
                    relief='raised',
                    height=2,
                    width=25)
@@ -178,8 +179,8 @@ header3.pack(side='left', fill='both')
 header31 = tk.Label(div5,
                     text='Carpeta',
                     font=('Arial', 10, 'bold'),
-                    bg='#003153',
-                    fg='#F1C40F',
+                    bg='#134351',
+                    fg='#FFBB05',
                     justify='center',
                     relief='raised',
                     height=2,
@@ -190,11 +191,11 @@ header31.pack(side='left', fill='both', expand=True)
 #+++++++++++++++++++++++++       Dataframe    +++++++++++++++++++++++++#
 
 div6 = tk.LabelFrame(root,
-                     bg='#003153',
+                     bg='#858784',
                      relief='flat')
 div6.pack(fill='both', padx=10, expand=True)
 
-mycanvas = tk.Canvas(div6, bg='#003153', relief='flat')
+mycanvas = tk.Canvas(div6, bg='#134351', relief='flat')
 mycanvas.pack(side=LEFT, fill='both', expand=True)
 
 style = ttk.Style()
@@ -210,7 +211,7 @@ mycanvas.configure(yscrollcommand=yscroll.set)
 mycanvas.bind('<Configure>', lambda e: mycanvas.configure(
     scrollregion=mycanvas.bbox('all')))
 
-myframe = tk.Frame(mycanvas, bg='#003153', relief='flat')
+myframe = tk.Frame(mycanvas, bg='#134351', relief='flat')
 mycanvas.create_window((0, 0), window=myframe, anchor='nw')
 
 for n, val in enumerate(commands.brief(), 2):
@@ -219,7 +220,7 @@ for n, val in enumerate(commands.brief(), 2):
     webent = tk.Entry(myframe,
                       textvariable=data1,
                       font=('Arial', 11, 'bold'),
-                      bg='#003153',
+                      bg='#134351',
                       foreground='white',
                       width=25)
     webent.grid(row=n, column=0, sticky='ew', pady=5)
@@ -229,7 +230,7 @@ for n, val in enumerate(commands.brief(), 2):
     webent2 = tk.Entry(myframe,
                       textvariable=data2,
                       font=('Arial', 11, 'bold'),
-                      bg='#003153',
+                      bg='#134351',
                       foreground='white',
                       width=74)
     webent2.grid(row=n, column=1, sticky='ew')
