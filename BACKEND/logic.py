e86cb7df08efd272f60f962d42dde68966d86c16
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-07-30 16:59:59 -0500

    masiveDump function creation

--- BACKEND/logic.py
+++ BACKEND/logic.py
@@ -91,9 +91,52 @@ def dropPlace(root, cityname):
         with open(root, 'w') as file:
             json.dump(newcont, file, indent=4)
     return n
+
+
+def attsFromFile(pathfile, sep=None, headers=False, iloc=[1, 2], encod='utf-8'):
+    """
+    Get a dictionary type object with the attributes from a csv file for every
+    new record to be added into cities forecasted database.
+    CSV file must have 2 columns at least: one with the url and another one
+    with the save targetpath where will be saved the data scrapped from the
+    web.
+
+    Parameters:
+    <<pathfile>> a string with the path file.
+    
+    <<sep>> character which separate every field. Is not define by default.
+
+    <<headers>> indicates if the file have headers. By default is False.
+
+    <<iloc>> is a list type object with the column number where is every
+    corresponding attribute. The order of indexes corresponds as follow:
+    [url, folder, name]. By default takes the order [1, 2].
+    For example:
+    [2,1] indicates that url is in the second column and the targetpath folder
+    information is in the column number one. 
+    
+    <<encod>> File encoding. By defaul is UTF-8.
+
+    """
+
+    attributes = {}
+    
+    with open(pathfile, 'r', encoding=encod) as file:
+        lines = (line.split(sep) for line in file)
+        if headers:
+            next(lines)
+        for line in lines:
+            atts = [att.strip("\n\ufeff") for att in line]
+            
+            attributes['url'] = atts[iloc[0]-1]
+            attributes['name'] = atts[iloc[1]-1]
+            yield attributes
+
 #--------------------------------------------------------------#
 
 
 if __name__ == '__main__':
-    newPlace(url=os.getenv('starturl'), root=os.getenv(
-        'root'), targetpath=os.getenv('testdir'), cityname='sma')
+    # newPlace(url=os.getenv('starturl'), root=os.getenv(
+    #     'root'), targetpath=os.getenv('testdir'), cityname='sma')
+    pipline = ((att['url'], att['name']) for att in attsFromFile('c:/usersfrancisco ruiz/desktop/example.csv',','))
+    print(next(pipline))
