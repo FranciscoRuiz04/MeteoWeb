5a4e32e932275d10dd0c7570005d741b419b011e
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-08-02 15:03:39 -0500

    Fields Separator section

--- FRONTEND/importwin.py
+++ FRONTEND/importwin.py
@@ -45,14 +45,36 @@ div3.pack(padx=6, fill='x')
 div4 = wdg.LabelFrame(root)
 div4.pack(fill='x', padx=10, pady=10)
 
-# # # Define empty variables
-# var1 = tk.IntVar()
-# var2 = tk.IntVar()
+h2 = wdg.SectionName(div4, 'Separador de Campos')
+h2.grid(row=0, column=0, padx=10, pady=3)
 
-# # # Define a Checkbox
-# t1 = tk.Checkbutton(div4, text="Python", variable=var1, onvalue=1, offvalue=0)
-# t1.pack()
-# t2 = tk.Checkbutton(div4, text="C++", variable=var2, onvalue=1, offvalue=0)
-# t2.pack()
+
+# Define empty variables
+vc1 = tk.IntVar()
+vc2 = tk.IntVar()
+vc3 = tk.IntVar()
+
+#-------------------------   Checkbox   ---------------------------
+#Comas
+c1 = wdg.CB(div4, 'Coma', lambda:wdg.checkBx(vc1, c2, c3), vc1)
+c1.grid(row=1, column=0, padx=40)
+
+#Tab
+c2 = wdg.CB(div4, 'Tabulación', lambda:wdg.checkBx(vc2, c1, c3), vc2)
+c2.grid(row=1, column=1, padx=60)
+
+#Other
+c3 = wdg.CB(div4, 'Otro', lambda: wdg.checkBx(vc3, c1, c2), vc3)
+c3.grid(row=1, column=2, padx=10)
+#Separator Value
+separator = wdg.Entry(div4, 3, isbold=True)
+separator.grid(row=1, column=3)
+
+
+
+
+
+
+
 
 root.mainloop()
