cb996ba375dd4e60ea7f69eedab52cf2390f4258
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-08-21 09:50:10 -0500

    File_Brief creator creation

--- BACKEND/meteoweb/creators.py
+++ BACKEND/meteoweb/creators.py
@@ -1,12 +1,16 @@
 ########################    Packages    ########################
+
 from datetime import datetime
 import os
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
 #-----------------------    GPS Pckgs    ----------------------#
 from meteoweb import collectors
+# import collectors
 #--------------------------------------------------------------#
 
 
+########################    Function    ########################
 
 def isredundant(dfObj, pathfolder):
     try:
@@ -21,8 +25,10 @@ def isredundant(dfObj, pathfolder):
             if newRow != oldRow:
                 return False
         return True
+#--------------------------------------------------------------#
 
 
+########################    Classes    ########################
 
 class File_24H:
 
@@ -42,7 +48,7 @@ class File_24H:
         name = self.name + '_C' + creation
         filename = name + '.' + self.format
         self.filename = filename
-    
+
     def FileDir(self, mainFolderName='24H'):
         self.Filename()
         childfolder = self.tp + os.sep + mainFolderName
@@ -53,7 +59,7 @@ class File_24H:
         self.FileDir()
         filepath = self.filedir + os.sep + self.filename
         self.filepath = filepath
-    
+
     def NewFile(self, obj=collectors.DailyArray):
         self.FilePath()
         directory = self.filedir
@@ -68,10 +74,10 @@ class File_24H:
 
 
 class File_3H(File_24H):
-    
+
     def __init__(self, placeAtts, format, encoding):
         super().__init__(placeAtts, format, encoding)
-    
+
     def Filenames(self):
         super().Filename()
         n = int(self.day[:2])
@@ -81,13 +87,13 @@ class File_3H(File_24H):
             names.append(init)
             n += 1
         self.filenames = names
-    
+
     def FileDir(self, mainFolderName='3H'):
         super().FileDir(mainFolderName)
         self.Filenames()
         pathcomps = [self.filedir, self.time]
         self.filedir = os.sep.join(pathcomps)
-        
+
     def FilePaths(self):
         self.FileDir()
         filepaths = []
@@ -95,8 +101,7 @@ class File_3H(File_24H):
             filepath = self.filedir + os.sep + name
             filepaths.append(filepath)
         self.filepaths = filepaths
-            
-    
+
     def NewFile(self, obj=collectors.H3Array):
         self.FilePaths()
         directory = self.filedir
@@ -111,21 +116,54 @@ class File_3H(File_24H):
 
 
 class File_1H(File_3H):
-    
+
     def __init__(self, placeAtts, format, encoding):
         super().__init__(placeAtts, format, encoding)
-    
+
     def FileDir(self, mainFolderName='1H'):
         super().FileDir(mainFolderName)
-    
+
     def NewFile(self, obj=collectors.H1Array):
         return super().NewFile(obj)
 
 
+class File_Brief(File_24H):
+
+    def __init__(self, placeAtts, format, encoding):
+        super().__init__(placeAtts, format, encoding)
+
+    def Filename(self):
+        creation = datetime.now().strftime("%d%m%y_%H%M")
+        self.day = creation.split('_')[0]
+        self.time = creation.split('_')[1]
+        name = 'C' + creation
+        filename = name + '.' + self.format
+        self.filename = filename
+
+    def FileDir(self, mainFolderName='Resumenes'):
+        self.Filename()
+        childfolder = self.tp.split(os.sep)
+        self.filedir = os.sep.join(childfolder[:-1]) + os.sep + mainFolderName
+        if not os.path.exists(self.filedir):
+            os.makedirs(self.filedir)
+    
+    def FormatRecords(self):
+        collector = collectors.Brief(self.url)
+        for i,url in enumerate(collector.urls):
+            if i == 6: last = True
+            else: last = False
+            meteoData = collector.genArray(url, last)
+            meteoData.insert(0, self.name)
+            yield meteoData
+#--------------------------------------------------------------#
 
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     from dotenv import load_dotenv as env
     env()
-    ini = File_1H(os.getenv('json'), 'txt', 'utf-8')
-    ini.NewFile()
+    ini = File_Brief({"path": "C:\\DailyForecast_test\\gto_capital",
+        "url": "https://www.meteoblue.com/es/tiempo/semana/guanajuato_m%c3%a9xico_4005270",
+        "city": "gto_capital"}, 'txt', 'utf-8')
+    import sys
+    print(sys.getsizeof(next(ini.FormatFile())))
