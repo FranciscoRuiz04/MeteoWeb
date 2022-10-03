05f25c93bde1c7f7224a13357a73229eb6d1d3e1
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-10-03 14:04:39 -0500

    Path bug at importation module

--- FRONTEND/winds/main.py
+++ FRONTEND/winds/main.py
@@ -13,17 +13,17 @@ from tkinter import LEFT, RIGHT, ttk
 
 #-----------------------    GPS Pckgs    ----------------------#
 ## Module importation to exec file creation and distribution
-from FRONTEND import commands
-from FRONTEND import widgets as wdg
+# from FRONTEND import commands
+# from FRONTEND import widgets as wdg
 
 ## Module importation to be developing
-# try:
-#     import widgets as wdg
-# except ModuleNotFoundError:
-#     import sys
-#     sys.path.append(r'C:\CODES\MeteoWeb\FRONTEND')
-#     import widgets as wdg
-# import commands
+try:
+    import widgets as wdg
+except ModuleNotFoundError:
+    import sys
+    sys.path.append(r'C:\CODES\MeteoWeb\FRONTEND')
+    import widgets as wdg
+import commands
 #--------------------------------------------------------------#
 def win(root):
     # Root window
