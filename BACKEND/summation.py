66193cdf78c0d2118e10c6198c01be53826a5fe9
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-10-26 11:34:20 -0500

    Update file name

--- BACKEND/summation.py
+++ BACKEND/summation.py
@@ -45,7 +45,7 @@ def exec14(gen, targetpath):
         numrecs += 14
         
     objClass.Filename()
-    fname = 'Concentrado_14' + "_" + objClass.filename
+    fname = 'Concentrado14' + "_" + objClass.filename
     targetpath += os.sep + fname
     df.to_csv(targetpath, encoding='utf-8',index_label='ID')
     return {"nrecs":numrecs, "filedir":os.path.dirname(targetpath), "filename":os.path.basename(targetpath)}
