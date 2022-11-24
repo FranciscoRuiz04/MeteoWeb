3fe93d14a3458b538bd7fb5db8fa77fa6934e881
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-11-24 09:12:34 -0600

    Drop duplicated index

--- BACKEND/summation.py
+++ BACKEND/summation.py
@@ -48,7 +48,7 @@ def exec14(gen, targetpath):
     fname = 'Concentrado14' + "_" + objClass.filename
     targetpath += os.sep + fname
     
-    df.reset_index(inplace=True) # Patch bug at index order since index repeat for every city
+    df.reset_index(inplace=True, drop=True) # Patch bug at index order since index repeat for every city
     df.to_csv(targetpath, encoding='utf-8',index_label='ID')
     return {"nrecs":numrecs, "filedir":os.path.dirname(targetpath), "filename":os.path.basename(targetpath)}
 
