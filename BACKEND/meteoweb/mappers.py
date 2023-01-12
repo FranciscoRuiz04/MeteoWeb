c5c91c1ae9d63fe780d8c59861a30cad5638e549
Francisco Ruiz <franciscoruiz078@gmail.com>
2023-01-12 17:50:00 -0600

    Mapping functionality initial code

--- BACKEND/meteoweb/mappers.py
+++ BACKEND/meteoweb/mappers.py
@@ -1,76 +1,24 @@
 ########################    Packages    ########################
-
-import matplotlib.pyplot as plt
-import geopandas as geopd
 import pandas as pd
+import geopandas as gpd
 import os
+from dotenv import load_dotenv as env
+env()
+
 #-----------------------    GPS Pckgs    ----------------------#
-# Module importation to exec file creation
-from . import collectors
-from . import scrappers as scr
-
-# Module importation to be developing
-# import scrappers as scr
-# import collectors
-#--------------------------------------------------------------#
+import creators as cre
 
 
-class Attribute(collectors.Brief):
-
-    def __init__(self, url):
-        super().__init__(url)
-        self.coords = scr.Daily4cast(url).coords
-
-    def baseArray(self, url, last=False):
-        fulldata = super().baseArray(url, last)
-        outcome = fulldata[:2]
-        return outcome
-
-    def genArray(self, url, last=False):
-        parameters = super().genArray(url, last)
-        row = parameters[1:] + self.coords
-        names = ['Accum_P', 'Tmp_Min', 'Tmp_Max', 'Lat', 'Lon', 'Elev']
-        outcome = {names[n]: value for n, value in enumerate(row)}
-        return outcome
-
-
-class AttributesTable:
-
-    def __init__(self, attObj, genObj):
-        self.__genObj = genObj(os.getenv('root'))
-        self.__attObj = attObj
-
-    def records(self):
-        for loc in self.__genObj:
-            obj = self.__attObj(loc['url'])
-            record = obj.genArray(loc['url'])
-            record['Loc'] = loc['city']
-            yield record
-
-    def genTable(self, export=False):
-        atts = pd.DataFrame()
-        for att in self.records():
-            atts = atts.append(att, ignore_index=True)
-        attTable = geopd.GeoDataFrame(atts,
-                                      geometry=geopd.points_from_xy(atts['Lon'],
-                                                                    atts['Lat'],
-                                                                    atts['Elev']))
-        if export:
-            attTable.to_file('C:\DailyForecast_test\muns_wgs84.shp', driver='ESRI Shapefile')
-        else:
-            return attTable
-
-    def mapping(self):
-        attTable = self.genTable()
-        attTable.plot()
-        plt.show()
-
-if __name__ == '__main__':
-    import sys
-    from dotenv import load_dotenv as env
-    env()
-    sys.path.append(os.getenv('BACKENDMods'))
-    import logic
+def urlCoords():
+    """
+    A generator to create a new URL for every coordindates pair
+    extracted from stations shapefile.
+    """
     
-    ini = AttributesTable(attObj=Attribute, genObj=logic.getPlaces)
-    ini.genTable(True)
+    shp = gpd.read_file(os.getenv('shp'))
+    coords = (dict(north=row['POINT_Y'], east=row['POINT_X']) for _, row in shp.iterrows()) #Generator object with every coordinates pair
+    for coord in coords:
+        north = str(coord['north'])[:5] #Take only the two first decimals
+        east = str(coord['east'])[:7]   #Take only the two first decimals
+        url = "{}/es/tiempo/semana/{}N{}E".format(os.getenv('mainurl'), north, east)
+        yield dict(url=url, city=(float(east), float(north)),path='ram')
